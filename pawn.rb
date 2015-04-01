57c833529c918bad93ebbd5a8882cf6018ed91b4
Robin Horton <robin.yerkes.horton@gmail.com>
2015-04-01 17:58:59 -0400

    add special Pawn moves; need to check

--- pawn.rb
+++ pawn.rb
@@ -10,29 +10,30 @@ class Pawn < Piece # < SteppingPieces
   def moves
     moves = []
 
-    # # pawn can only step one space forward
-    # w_move = [self.position[0] + 1, self.position[1]]
-    # b_move = [self.position[0] - 1, self.position[1]]
+    # pawn can only step one space forward
+    w_move = [self.position[0] + 1, self.position[1]]
+    b_move = [self.position[0] - 1, self.position[1]]
+
+    moves << (self.color == :w ? [w_move] : [b_move])
+
+    # pawn can take two steps forward at first move (only)
+    if self.color == :w && self.position[0] == 1
+      moves << [self.position[0] + 2, self.position[1]]
+    elsif self.color == :b && self.position[0] == 6
+      moves << [self.position[0] - 2, self.position[1]]
+    end
     #
-    # moves << (self.color == :w ? [w_move] : [b_move])
-    #
-    # # pawn can take two steps forward at first move (only)
-    # if self.color == :w && self.position[0] == 1
-    #   moves << [self.position[0] + 2, self.position[1]]
-    # elsif self.color == :b && self.position[0] == 6
-    #   moves << [self.position[0] - 2, self.position[1]]
-    # end
-    #
-    # # pawn can step diagonally when taking an opponent's piece
-    # if self.color == :w && @board[self.position[0] + 1, self.position[1] + 1].color == :b
-    #   moves << [self.position[0] + 1, self.position[1] + 1]
-    # elsif self.color == :w && @board[self.position[0] + 1, self.position[1] - 1].color == :b
-    #   moves << [self.position[0] + 1, self.position[1] - 1]
-    # elsif self.color == :b && @board[self.position[0] - 1, self.position[1] + 1].color == :w
-    #   moves << [self.position[0] + 1, self.position[1] + 1]
-    # elsif self.color == :b && @board[self.position[0] - 1, self.position[1] - 1].color == :w
-    #   moves << [self.position[0] + 1, self.position[1] - 1]
-    # end
+    # pawn can step diagonally when taking an opponent's piece
+
+    if self.color == :w && @board[[self.position[0] + 1, self.position[1] + 1]] && @board[[self.position[0] + 1, self.position[1] + 1]].color == :b
+      moves << [self.position[0] + 1, self.position[1] + 1]
+    elsif self.color == :w && @board[[self.position[0] + 1, self.position[1] - 1]] && @board[[self.position[0] + 1, self.position[1] - 1]].color == :b
+      moves << [self.position[0] + 1, self.position[1] - 1]
+    elsif self.color == :b && @board[[self.position[0] - 1, self.position[1] + 1]] && @board[[self.position[0] - 1, self.position[1] + 1]].color == :w
+      moves << [self.position[0] + 1, self.position[1] + 1]
+    elsif self.color == :b && @board[[self.position[0] - 1, self.position[1] - 1]] && @board[[self.position[0] - 1, self.position[1] - 1]].color == :w
+      moves << [self.position[0] + 1, self.position[1] - 1]
+    end
 
     moves
   end
